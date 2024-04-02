da99d0743c78dd72bd98a08ed247b025051b5654
tarunclub <tarunkm__@outlook.com>
2024-04-02 18:17:23 +0530

    problems init

---
+++ apps/web/app/problems/[id]/page.tsx
@@ -0,0 +1,11 @@
+import ProblemDescription from '../../../components/ProblemDescription';
+import Playground from '../../../components/Playground';
+
+export default function ProblemPage(): JSX.Element {
+  return (
+    <div>
+      <ProblemDescription />
+      <Playground />
+    </div>
+  );
+}
---
+++ apps/web/components/ProblemDescription.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ProblemDescription() {
+  return <div>Problem description</div>;
+}
